7c3db93bc02a8d4ef0f7ea26afb6b67a1765b49f
Sanjay Yepuri <sanjay.yepuri@gmail.com>
2020-03-25 19:55:04 -0500

    basic implementation of firebase auth

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,6 +3,7 @@ import { Router } from "@reach/router";
 import NotFound from "./pages/NotFound.js";
 import "../utilities.css";
 import Homepage from "./pages/Homepage.js";
+import Authpage from "./pages/Authpage.js";
 
 /**
  * Define the "App" component as a class.
@@ -19,6 +20,9 @@ class App extends Component {
         <Router>
           <Homepage
             path="/"
+          />
+          <Authpage
+            path="/auth"
           />
           <NotFound default />
         </Router>
---
+++ client/src/components/pages/Authpage.js
@@ -0,0 +1,97 @@
+import React, { Component } from "react";
+
+import "../../utilities.css";
+import "./Homepage.css";
+import { get } from "../../utilities";
+
+// auth 
+import firebase, {auth} from "../../firebase-config";
+
+class Authpage extends Component {
+  constructor(props) {
+    super(props);
+    // Initialize Default State
+    this.state = {ok: false, user: undefined, error: undefined};
+    this.state.form = {email: undefined, password: undefined};
+
+    this.signup = this.signup.bind(this);
+    this.signin = this.signin.bind(this);
+    this.signout = this.signout.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+
+    auth.onAuthStateChanged((user) => {
+        this.setState({
+            error: undefined,
+            user : user
+        });
+    });
+  }
+
+  
+  async signup() {
+    event.preventDefault();
+    try {
+        const user = await auth.createUserWithEmailAndPassword(this.state.form.email, this.state.form.password)
+        console.log(user);
+    } catch (error) {
+        console.log(error);
+        this.setState({error: error});
+    }
+  }
+
+  async signin(event) {
+    event.preventDefault();
+    try {
+        const user = await auth.signInWithEmailAndPassword(this.state.form.email, this.state.form.password);
+        console.log(user);
+    } catch(error) {
+        console.log(error);
+        this.setState({error: error});
+    }
+  }
+
+  signout() {
+      auth.signOut();
+  }
+
+  handleChange(event) {
+      const form = this.state.form;
+      form[event.target.name] = event.target.value
+      this.setState({form: form});
+  }
+
+  render() {
+    return (
+        <>
+        <h1>Coved Firebase Auth Test</h1>
+        <pre style={{ height: 300, overflow: "auto" }}>
+            {JSON.stringify(this.state, null, 2)}
+        </pre>
+            
+            <form>
+                <label>
+                    Email:
+                    <input type="text" name="email" 
+                        value={this.state.form.email} 
+                        onChange={this.handleChange}
+                    />
+                </label>
+                <label>
+                    Password:
+                    <input type="password" name="password" 
+                        value={this.state.form.password} 
+                        onChange={this.handleChange}
+                    />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+
+            <button onClick={this.signup}>Sign Up</button>
+            <button onClick={this.signin}>Sign In</button>
+            <button onClick={this.signout}>Sign Out</button>
+        </>
+    );
+  }
+}
+
+export default Authpage;
