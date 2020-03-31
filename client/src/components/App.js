2f929f49c814b0f787b2bf9dd54bcfe216ad60d1
Sanjay Yepuri <sanjay.yepuri@gmail.com>
2020-03-30 20:03:09 -0500

    added back UserProvider

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -37,19 +37,20 @@ class App extends Component {
           integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
           crossorigin="anonymous"
         />
-        <NavBar />
-        <Router basepath="/">
-          <Homepage path="/" />
-          <Resources path="/resources" />
-          <Authpage path="/auth" />
-          <Register path="/register" />
-          <FAQ path="/faq" />
-          <WhoWeAre path="/whoweare" />
-          <Contact path="/contact" />
-          <FindATutor path="/findatutor"/>
-          <NotFound default></NotFound>
-        </Router>
-        
+        <UserProvider>
+          <NavBar />
+          <Router basepath="/">
+            <Homepage path="/" />
+            <Resources path="/resources" />
+            <Authpage path="/auth" />
+            <Register path="/register" />
+            <FAQ path="/faq" />
+            <WhoWeAre path="/whoweare" />
+            <Contact path="/contact" />
+            <FindATutor path="/findatutor"/>
+            <NotFound default></NotFound>
+          </Router>
+        </UserProvider>
       </>
     );
   }
