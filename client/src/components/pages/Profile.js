7e00084c7feec021965a7d9c60a53643d06d6ce4
April Xie <aprilxie@mit.edu>
2020-03-31 18:30:14 -0700

    added conditional rendering for profile

--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -1,16 +1,23 @@
 import React, { Component } from "react";
 import TuteeProfile from "../modules/TuteeProfile.js";
+import TutorProfile from "../modules/TutorProfile.js";
 import "./Profile.css";
+import { UserContext } from "../../providers/UserProvider";
 
 class Profile extends Component {
   constructor(props) {
     super(props);
+    this.state = {user: {role: "tutee"}}; // eventually replace this once UserContext works
   }
 
+
+
   render() {
+    this.state.user = this.content;
+    let profile = this.state.user.role = "tutee" ? <TuteeProfile></TuteeProfile> : <TutorProfile></TutorProfile>;
     return (
       <div className='Profile-form'>
-        <TuteeProfile></TuteeProfile>
+        {profile}
       </div>
     );
   }
