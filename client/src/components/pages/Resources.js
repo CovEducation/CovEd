261f2fc1e26256c2b6a79f5d6c3a5b994c1fd713
Dheekshita Kumar <dheekshita@Dheekshitas-MacBook-Pro-4.local>
2020-04-06 00:17:27 -0400

    update resources

--- client/src/components/pages/Resources.js
+++ client/src/components/pages/Resources.js
@@ -19,6 +19,12 @@ import {
   CallToAction, Feature,
 } from "react-landing-page";
 
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
 const theme={
   colors: {
       blue: '#00568C',
