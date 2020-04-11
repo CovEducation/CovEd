38b200ec480bef5215463fd8c6323b7437a19ff4
Dheekshita Kumar <dheekshita@Dheekshitas-MacBook-Pro-4.local>
2020-04-10 22:02:15 -0400

    add complaints

--- client/src/components/pages/Contact.js
+++ client/src/components/pages/Contact.js
@@ -58,7 +58,9 @@ class Contact extends Component {
             <h2><span className="light-h2">Contact Us <br /><br /><hr className="hr-primary"/></span></h2>
             <br />
             <br />
-            <p>In the future we will have a contact form here, but for now...Check out the <a className="dark-a" href="/faq"> FAQs </a> page to see if we've already answered your question. If you don't see your question there, shoot us an email <a className="dark-a" href='mailto:coveducation@gmail.com'>coveducation@gmail.com</a>! We're excited to hear from you~</p>
+            <p>Check out the <a className="dark-a" href="/faq"> FAQs </a> page to see if we've already answered your question. If you don't see your question there, for general questions, please email us at <a className="dark-a" href='mailto:coveducation@gmail.com'>coveducation@gmail.com</a>! For any inquiries related to media/news releases, please contact us at <a className="dark-a" href='mailto:covedpr@gmail.com'>covedpr@gmail.com</a>. We're excited to hear from you~</p>
+            <p>If you wish to file a complaint, please contact <a className="dark-a" href='mailto:coved.management@gmail.com'>coved.management@gmail.com</a>
+            </p>
           </Col>
           </Row>
         </Section>
