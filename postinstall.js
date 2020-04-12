aeee1d0bc6023104cbcb0a90c9f8cbfefdf24a1c
Johan Cervantes <45345485+johancc@users.noreply.github.com>
2020-04-12 13:18:26 -0700

    Added callback

--- postinstall.js
+++ postinstall.js
@@ -1,4 +1,6 @@
 const fs = require("fs")
 require("dotenv").config()
 
-fs.writeFile("./google-credentials-heroku.json", process.env.GOOGLE_CONFIG, (err) => {})
+fs.writeFile("./google-credentials-heroku.json", process.env.GOOGLE_CONFIG, (err) => {
+  console.log("Credentials written");
+})
