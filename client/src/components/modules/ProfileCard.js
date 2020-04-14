d297c4d47dc460329ef2be661a9857fdf0019ec9
Johan Cervantes <45345485+johancc@users.noreply.github.com>
2020-04-13 22:54:35 -0700

    Removed unused import

--- client/src/components/modules/ProfileCard.js
+++ client/src/components/modules/ProfileCard.js
@@ -5,8 +5,6 @@ import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
 import Divider from "@material-ui/core/Divider";
-import { useFadedShadowStyles } from "@mui-treasury/styles/shadow/faded";
-import { useGutterBorderedGridStyles } from "@mui-treasury/styles/grid/gutterBordered";
 
 const useStyles = makeStyles(({ palette }) => ({
   card: {
