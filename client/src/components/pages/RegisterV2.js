bcebc895faa97737d298793d30f891f95dbbacb7
Sanjay Yepuri <sanjay.yepuri@gmail.com>
2020-04-20 01:17:38 -0500

    made form validation work for all fields

--- client/src/components/pages/RegisterV2.js
+++ client/src/components/pages/RegisterV2.js
@@ -31,12 +31,15 @@ const getNameField = (formik) => {
       <Form.Label>Name</Form.Label>
       <Form.Control
         name="name"
-        value={formik.values.name}
+        {...formik.getFieldProps('name')}
+        isInvalid={formik.touched.name && formik.errors.name}
         required
         type="text"
         placeholder="Your Name"
-        onChange={formik.handleChange} />
-      <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+      />
+      <Form.Control.Feedback type="invalid">
+        {formik.errors.name}
+      </Form.Control.Feedback>
     </Form.Group>
   )
 }
@@ -47,16 +50,16 @@ const getEmailField = (formik) => {
       <Form.Label>Email</Form.Label>
       <Form.Control
         name="email"
-        value={formik.values.email}
-        onChange={formik.handleChange}
+        {...formik.getFieldProps('email')}
+        isInvalid={formik.touched.email && formik.errors.email}
         type="email"
         placeholder="user@domain.com"
         aria-describedby="inputGroupPrepend"
         required
       />
       <Form.Control.Feedback type="invalid">
-        Please input a valid  email.
-            </Form.Control.Feedback>
+        {formik.errors.email}
+      </Form.Control.Feedback>
     </Form.Group>
   )
 }
@@ -66,16 +69,19 @@ const getTimezoneField = (formik) => {
     <Form.Group as={Col} controlId="timezone-select">
       <Form.Label>Time Zone</Form.Label>
       <Form.Control
-        name="timezone"
-        value={formik.values.timeZone}
+        name="timeZone"
+        {...formik.getFieldProps('timeZone')}
+        isInvalid={formik.touched.timeZone && formik.errors.timeZone}
         as="select"
-        onChange={formik.handleChange}
       >
         {timeZones.map((tz => {
           return (
             <option value={tz.value}> {tz.timezone} </option>
           )
         }))}
+        <Form.Control.Feedback type="invalid">
+          {formik.errors.timeZone}
+        </Form.Control.Feedback>
       </Form.Control>
     </Form.Group>
   )
@@ -89,34 +95,32 @@ const getPasswordField = (formik) => {
         <InputGroup>
           <Form.Control
             name="password"
-            value={formik.values.password}
-            onChange={formik.handleChange}
+            {...formik.getFieldProps('password')}
+            isInvalid={formik.touched.password && formik.errors.password}
             type="password"
             placeholder="Password"
             aria-describedby="inputGroupPrepend"
-            required
-            pattern="^[A-Za-z0-9\s$&+,:;=?@#|'<>.^*()%!-]{6,}$"
           />
           <Form.Control.Feedback type="invalid">
-            Password must be at least 6 characters long.
-            </Form.Control.Feedback>
+            {formik.errors.password}
+          </Form.Control.Feedback>
         </InputGroup>
       </Form.Group>
       <Form.Group as={Col} md="6" controlId="validationPassword1">
         <Form.Label>Confirm password</Form.Label>
         <InputGroup>
           <Form.Control
-            name="confirmPassword"
+            name="passwordConfirmation"
+            {...formik.getFieldProps('passwordConfirmation')}
+            isInvalid={formik.touched.passwordConfirmation && formik.errors.passwordConfirmation}
             type="password"
             placeholder="Reenter Password"
             aria-describedby="inputGroupPrepend"
             required
-            value={formik.values.passwordConfirmation}
-            onChange={formik.handleChange}
           />
           <Form.Control.Feedback type="invalid">
-            Passwords do not match.
-            </Form.Control.Feedback>
+            {formik.errors.passwordConfirmation}
+          </Form.Control.Feedback>
         </InputGroup>
       </Form.Group>
     </>
@@ -129,12 +133,15 @@ const getRoleField = (formik) => {
       <Form.Label>Role</Form.Label>
       <Form.Control
         name="role"
-        value={formik.values.role}
-        onChange={formik.handleChange}
+        {...formik.getFieldProps('role')}
+        isInvalid={formik.touched.role && formik.errors.role}
         as="select">
         <option value="student">Student/Parent</option>
         <option value="mentor">Mentor</option>
       </Form.Control>
+      <Form.Control.Feedback type="invalid">
+        {formik.errors.role}
+      </Form.Control.Feedback>
     </Form.Group>
   )
 }
@@ -144,10 +151,13 @@ const getBioField = (formik) => {
     <Form.Group as={Col} controlId="formBioTextArea">
       <Form.Label>Introduce Yourself!</Form.Label>
       <Form.Control name="bio"
-        value={formik.values.bio}
+        {...formik.getFieldProps('bio')}
+        isInvalid={formik.touched.bio && formik.errors.bio}
         as="textarea"
-        rows="3"
-        onChange={formik.handleChange} />
+        rows="3" />
+      <Form.Control.Feedback type="invalid">
+        {formik.errors.bio}
+      </Form.Control.Feedback>
     </Form.Group>
   )
 }
@@ -156,12 +166,22 @@ const getSubjectField = (formik) => {
   return (
     <Form.Group as={Col} controlId="exampleForm.ControlSelect2">
       <Form.Label>Subjects</Form.Label>
+      
       <Select
         value={formik.values.subjects}
+        className={!!formik.errors.subjects && formik.touched.subjects ?"is-invalid":""}
+        onChange={selected => formik.setFieldValue("subjects", selected)}
+        isInvalid={formik.touched.subjects && formik.errors.subjects}
+        onBlur={() => formik.setFieldTouched("subjects")}
         options={subjects}
         isMulti
-        onChange={selected => formik.setFieldValue("subjects", selected)} />
+      />
+      <div className="invalid-feedback">
+        {formik.errors.subjects} 
+      </div>
+     
     </Form.Group>
+    
   )
 }
 
@@ -171,9 +191,16 @@ const getTagField = (formik) => {
       <Form.Label>Optional tags: </Form.Label>
       <Select
         value={formik.values.tags}
+        className={!!formik.errors.tags && formik.touched.tags ?"is-invalid":""}
+        onChange={selected => formik.setFieldValue("tags", selected)}
+        isInvalid={formik.touched.tags && formik.errors.tags}
+        onBlur={() => formik.setFieldTouched("tags")}
         options={tags}
         isMulti
-        onChange={selected => formik.setFieldValue("tags", selected)} />
+      />
+      <div className="invalid-feedback">
+        {formik.errors.tags} 
+      </div>
     </Form.Group>
   )
 }
@@ -186,13 +213,15 @@ const getMentorFields = (formik) => {
         <InputGroup>
           <Form.Control
             name="major"
-            value={formik.values.major}
+            {...formik.getFieldProps('major')}
+            isInvalid={formik.touched.major && formik.errors.major}
             type="text"
             placeholder="Learning"
             aria-describedby="inputGroupPrepend"
-            required
-            onChange={formik.handleChange}
           />
+          <Form.Control.Feedback type="invalid">
+            {formik.errors.major}
+          </Form.Control.Feedback>
         </InputGroup>
       </Form.Group>
     </>
@@ -204,29 +233,30 @@ const getMenteeFields = (formik) => {
     <Form.Row>
       <Form.Group as={Col} md="6" controlId="validationGuadianName">
         <Form.Label>Parent's Name</Form.Label>
-        <Form.Control 
-          name="guardian_name" 
-          value={formik.guardian_name} 
-          onChange={formik.handleChange} 
-          required type="text" 
+        <Form.Control
+          name="guardian_name"
+          {...formik.getFieldProps('guardian_name')}
+          isInvalid={formik.touched.guardian_name && formik.errors.guardian_name}
+          type="text"
           placeholder="Parent Name" />
-        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        <Form.Control.Feedback type="invalid">
+          {formik.errors.guardian_name}
+        </Form.Control.Feedback>
       </Form.Group>
       <Form.Group as={Col} md="6" controlId="validationEmail">
         <Form.Label>Parent Email</Form.Label>
         <InputGroup>
           <Form.Control
             name="guardian_email"
-            value={formik.values.guardian_email}
-            onChange={formik.handleChange}
+            {...formik.getFieldProps('guardian_email')}
+            isInvalid={formik.touched.guardian_email && formik.errors.guardian_email}
             type="email"
             placeholder="youremail@mail.com"
             aria-describedby="inputGroupPrepend"
-            required
           />
           <Form.Control.Feedback type="invalid">
-            Please input a valid email.
-              </Form.Control.Feedback>
+            {formik.errors.guardian_email}
+          </Form.Control.Feedback>
         </InputGroup>
       </Form.Group>
     </Form.Row>
@@ -234,22 +264,47 @@ const getMenteeFields = (formik) => {
 };
 
 
-const validateSchema = () => {
+const RegisterSchema =
   Yup.object().shape({
-    name: Yup.string()
-      .required('Required'),
-    email: Yup.string()
-      .email()
-      .required('Required'),
+    name: Yup.string().required('Please enter your name.'),
+    timeZone: Yup.string().required('Required'),
+    role: Yup.string().required('Required'),
+    bio: Yup.string(),
+    subjects: Yup.array().required('Please select atleast one subject'),
+    tags: Yup.array(),
+
+    email: Yup.string().email().required('Please input a valid email.')
+      .when("role", {
+        is: role => role === "mentor",
+        then: Yup.string().email().matches(/.+@*.edu/i, "Mentors are required to use an .edu email.")
+                  .required("Please input a valid .edu email.")
+      }),
+
     password: Yup.string()
       .required('Password Required'),
     passwordConfirmation: Yup.string()
-      .oneOf([Yup.ref('password'), null], 'Passwords must match'),
-    timeZone: Yup.string()
-      .required('Required'),
-    
-  })
-}
+      .oneOf([Yup.ref('password'), null], 'Passwords must match')
+      .required('Password Confirmation Required'),
+
+    guardian_name: Yup.string()
+      .when("role", {
+        is: role => role === "student",
+        then: Yup.string().required("Please enter a Parent or Guardian's name.")
+      }),
+    guardian_email: Yup.string()
+      .email()
+      .when("role", {
+        is: role => role === "student",
+        then: Yup.string().required("Please input a valid email.")
+      }),
+
+    major: Yup.string()
+      .when("role", {
+        is: role => role === "mentor",
+        then: Yup.string().required("Major is a required field.")
+      })
+  });
+
 
 
 const Register = () => {
@@ -258,9 +313,11 @@ const Register = () => {
       role: 'student',
       subjects: [],
     },
+    validationSchema: RegisterSchema,
     onSubmit: values => {
       console.log(JSON.stringify(values, null, 2));
     },
+
   });
 
   const fieldGetters = [
@@ -288,8 +345,8 @@ const Register = () => {
                 return <Form.Row key={i}>{field}</Form.Row>
               })
             }
-            
-            { roleFields }
+
+            {roleFields}
             <Button type="submit">Submit</Button>
           </Form>
         </div>
