dd7116c8b32ee7660f4765c1fde6184a4e0b7b06
Elaine Zhang <elainez@mit.edu>
2020-07-03 18:13:24 -0700

    Made changes to Navbar based on wireframe

--- client/src/components/modules/NavBar.js
+++ client/src/components/modules/NavBar.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { Nav, Navbar } from "react-bootstrap";
+import { Nav, Navbar, Container, Button, NavDropdown} from "react-bootstrap";
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
 import "./NavBar.css";
 // auth
@@ -22,33 +23,38 @@ class NavBar extends Component {
     if (!this.context.user) {
       userLinks = (
         <>
-          <Nav.Link as={Link} to="/auth">Sign In</Nav.Link>
-          <Nav.Link as={Link} to="/register">Register</Nav.Link>
+          <Nav.Link as={Link} to="/auth">Login</Nav.Link>
+          {/* To be changed with new button that pulls up registration modal */}
+          <Button href='/register' variant="nav"> Sign Up </Button>
         </>
       );
     } else {
       userLinks = (
         <>
-          <Nav.Link as={Link} to="/" onClick={this.signOut}>Sign Out</Nav.Link>
-          <Nav.Link as={Link} to="/findamentor">Find a Mentor</Nav.Link>
-          <Nav.Link as={Link} to="/profile">Profile</Nav.Link>
-          <Nav.Link as={Link} to="/speakerseries">Speaker Series</Nav.Link>
+          <AccountCircleIcon/>
+          <NavDropdown title="Tim Beaver" renderMenuOnMount={true}>
+            <NavDropdown.Item href="/profile">Dashboard</NavDropdown.Item>
+            <NavDropdown.Item style={{ color: 'red' }} href="/" onClick={this.signOut}>Sign Out</NavDropdown.Item>
+          </NavDropdown>
         </>
       )
     }
 
     return (
-      <Navbar collapseOnSelect fixed="sticky-top" expand="lg" variant="light">
-        <Navbar.Brand as={Link} to="/">CovEd</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto"/>
-          <Nav>
-          { userLinks }
+      <Navbar collapseOnSelect fixed="sticky-top" expand="lg" variant="light" display="flex">
+        <Container display="flex" flex-grow="1">
+          <Navbar.Brand as={Link} to="/">CovEd</Navbar.Brand>
+          {/* <Navbar.Toggle aria-controls="responsive-navbar-nav" /> */}
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav.Link as={Link} to="/resources">How It Works</Nav.Link>
             <Nav.Link as={Link} to="/resources">Resources</Nav.Link>
-            <Nav.Link as={Link} to="/faq">FAQ</Nav.Link>
-            <Nav.Link as={Link} to="/about">About Us</Nav.Link>
-          </Nav>
+            <Nav.Link as={Link} to="/faq">FAQs</Nav.Link>
+            <Nav.Link as={Link} to="/about">Meet Our Team</Nav.Link>
+            <Nav.Link as={Link} to="/about">Contact Us</Nav.Link>
+          </Navbar.Collapse>
+        </Container>
+        <Navbar.Collapse id="user-links" align-self="flex-end">
+        { userLinks }
         </Navbar.Collapse>
       </Navbar>
     );
--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App.js";
 import 'pace-js/themes/blue/pace-theme-minimal.css'
+import "@material-ui/icons/AccountCircle"
 
 // renders React Component "Root" into the DOM element with ID "root"
 ReactDOM.render(<App />, document.getElementById("root"));
