ec7c73a4d1dd85542a4aca95a76eae6362e85250
srung <srungaramdhathri@gmail.com>
2020-07-26 18:09:24 -0400

    speaker series updates

--- client/src/components/pages/Event.js
+++ client/src/components/pages/Event.js
@@ -25,13 +25,13 @@ class Event extends Component {
         <Section fontSize={[2]} bg="white" heading="" subhead="" p={[1,2,2,2]} mt={7} mb={7}>
           <Row className="justify-content-center">
           <Col sm={{span: 10}}  xs={{span:10}} className="text-center">
-            <h4><span className="webinar-name">Maximize Your Virtual Summer<br /><hr className="hr-primary"/></span></h4>
-            <p className="text-left">We are hosting a workshop to help you maximize your virtual summer as a part of our Speaker Series on July 2nd from 3:00 to 4:30pm EST, our panelists will go over tips and tricks to make your summer more interesting and productive!
+            <h4><span className="webinar-name">Grit: The Power of PAssion and Perseverance<br /><hr className="hr-primary"/></span></h4>
+            <p className="text-left">Join us on August 5th with Angela Duckworth, the cofounder and CEO of Character Lab, to learn about grit. 
                <br />
                <br />
                </p>
-            <p><b>To register to attend our upcoming workshop (July 2nd),
-               click <a className="dark-a" href='https://docs.google.com/forms/d/e/1FAIpQLSdsVJeiyND9-MtbXaO-3kjKnkEO6wJxlAkT4J7FveEXDXQe3w/viewform'>here</a></b></p>
+            <p><b>To register to attend our upcoming workshop (August 5th),
+               click <a className="dark-a" href='https://docs.google.com/forms/d/e/1FAIpQLSfg12XntmYX10sJ8rPZtafNGrnlT8bxWwQ10JlGTlmu0sVXDg/viewform'>here</a></b></p>
             <p>And checkout our <a className="dark-a" href="https://www.facebook.com/events/236350171151111/"> facebook event page!</a></p>
             <br />
             <p> Can't make the event? Don't worry! We'll be recording it, and we'll have more events in the future!</p>
@@ -47,14 +47,14 @@ class Event extends Component {
           <br />
           <br />
           <Row className="justify-content-center">
-          <Col sm={{span: 20}}  xs={{span:15}} className="text-center">
-            <h4><span className="webinar-name">The Science of Happiness and Mental Health<br /><hr className="hr-primary"/></span></h4>
-            <p className="text-left"> Join us for our Science of Happiness and Mental Health workshop with Dr. Laurie Santos from Yale University on July 10th from 3:00 to 4:00 EST.
+          <Col sm={{span: 10}}  xs={{span:10}} className="text-center">
+            <h4><span className="webinar-name">COVID-19: Biology and Global Ramifications<br /><hr className="hr-primary"/></span></h4>
+            <p className="text-left"> Join us for our Science of Happiness and Mental Health workshop with Dr. Robert Lue to learn more about the biological and global ramifications of the COVID-19 pandemic. 
                <br />
                <br />
                </p>
-            <p><b>To register to attend our upcoming workshop (July 10th),
-               click <a className="dark-a" href='https://docs.google.com/forms/d/e/1FAIpQLScXxmrt9fBnncEIg4UG2Bxb3zZjTYymOsr3j6aNFq-2R1pz3w/viewform'>here</a></b></p>
+            <p><b>To register to attend our upcoming workshop (Auhust 14th),
+               click <a className="dark-a" href='https://docs.google.com/forms/d/e/1FAIpQLSckgMY8_ZqKYhP6JTYNO8aoD87iuwvbqzsVrG9GaS25EHFxEg/viewform'>here</a></b></p>
             <p>And checkout our <a className="dark-a" href="https://www.facebook.com/events/236350171151111/"> facebook event page!</a></p>
             <br />
             <p> Can't make the event? Don't worry! We'll be recording it, and we'll have more events in the future!</p>
@@ -62,8 +62,7 @@ class Event extends Component {
             </Col>
             </Row>
             <Row className="justify-content-center">
-            <iframe src="https://drive.google.com/file/d/1wZgdzy9gPubIkueV2jGFEHiLq7ydkl63/preview" width="640" height="480"></iframe>
-            
+            <iframe src="https://drive.google.com/file/d/1wZgdzy9gPubIkueV2jGFEHiLq7ydkl63/preview" width="640" height="480"></iframe> 
           </Row>
         </Section>
         </Provider>
